d7badebf36af081da532a32e2504f0a46c6dd020
moronijensen <moronijensen@gmail.com>
2018-02-28 13:40:50 -0700

    Changed the system output text for test commit

--- app/src/main/java/project/hhn_mobile/MainActivity.java
+++ app/src/main/java/project/hhn_mobile/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("This is my commit!");
+        System.out.println("Changed the commit!");
     }
 }
