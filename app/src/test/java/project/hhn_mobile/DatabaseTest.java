153ab076dee9f3766bdfdfcb28594cb35618cc41
infiniteloop311 <jdfagerb@byui.edu>
2018-02-28 13:53:52 -0700

    Created two test classes "DatabaseTest" and "UserObjectTest" for future local unit tests.

---
+++ app/src/test/java/project/hhn_mobile/DatabaseTest.java
@@ -0,0 +1,22 @@
+package project.hhn_mobile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by joshua on 2/28/18.
+ */
+
+public class DatabaseTest {
+
+    @Test
+    public void ConnectionIsAlive() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void DatabaseReturnsAppointments() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ app/src/test/java/project/hhn_mobile/UserObjectTest.java
@@ -0,0 +1,17 @@
+package project.hhn_mobile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by joshua on 2/28/18.
+ */
+
+public class UserObjectTest {
+
+    @Test
+    public void UserInfoIsSaved() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
