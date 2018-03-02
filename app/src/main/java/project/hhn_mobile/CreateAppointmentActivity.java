afd37feab63a6d1dd5da640d3adae2fa7cf6c188
moronijensen <moronijensen@gmail.com>
2018-03-02 14:02:38 -0700

    Added create appointment activity and most of the layout

---
+++ app/src/main/java/project/hhn_mobile/CreateAppointmentActivity.java
@@ -0,0 +1,13 @@
+package project.hhn_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_appointment);
+    }
+}
