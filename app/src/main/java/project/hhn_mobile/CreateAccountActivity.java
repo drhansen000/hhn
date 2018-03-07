c53aeb1b446819e880962ee657f55364a42871a9
infiniteloop311 <jdfagerb@byui.edu>
2018-03-07 11:35:07 -0700

    Creating an account with an email and password is a success!

--- app/src/main/java/project/hhn_mobile/CreateAccountActivity.java
+++ app/src/main/java/project/hhn_mobile/CreateAccountActivity.java
@@ -1,18 +1,26 @@
 package project.hhn_mobile;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
     private String email;
     private String password;
+    public static final String TAG = "TAG: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,25 @@ public class CreateAccountActivity extends AppCompatActivity {
     }
 
     public void createAccount(View view) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
 
+                        // ...
+                    }
+                });
     }
 }
