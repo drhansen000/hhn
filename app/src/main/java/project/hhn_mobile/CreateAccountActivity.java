3f6601a93f236659c734994182c0e3edb79d63d6
infiniteloop311 <jdfagerb@byui.edu>
2018-03-07 11:21:15 -0700

    Tested if I could create a slot under appointments in the database, Successful. Started to add the authentication logic and connecting the sign in, sign up, and future appointment activities.

--- app/src/main/java/project/hhn_mobile/CreateAccountActivity.java
+++ app/src/main/java/project/hhn_mobile/CreateAccountActivity.java
@@ -1,13 +1,38 @@
 package project.hhn_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        Intent intent = getIntent();
+        email = intent.getStringExtra(MainActivity.EMAIL_MESSAGE);
+        password = intent.getStringExtra(MainActivity.PASS_MESSAGE);
+
+        EditText editText9 = findViewById(R.id.editText9);
+        EditText editText7 = findViewById(R.id.editText7);
+
+        editText9.setText(email);
+        editText7.setText(password);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void createAccount(View view) {
+
     }
 }
--- app/src/main/java/project/hhn_mobile/MainActivity.java
+++ app/src/main/java/project/hhn_mobile/MainActivity.java
@@ -1,10 +1,13 @@
 package project.hhn_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -13,18 +16,42 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EMAIL_MESSAGE = "project.hhn_mobile.EMAIL";
+    public static final String PASS_MESSAGE = "project.hhn_mobile.PASSWORD";
+
+    private FirebaseAuth mAuth;
+
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("test/test1");
+    DatabaseReference myRef = database.getReference("appointment/test");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Changed the commit!");
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signIn(View view) {
+        Intent intent = new Intent(this, FutureAppointmentsActivity.class);
+    }
+
+    public void signUp(View view) {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+
+        EditText editText = findViewById(R.id.editText);
+        EditText editText2 = findViewById(R.id.editText2);
+
+        String email = editText.getText().toString();
+        String password = editText2.getText().toString();
+
+        intent.putExtra(EMAIL_MESSAGE, email);
+        intent.putExtra(PASS_MESSAGE, password);
+        startActivity(intent);
     }
 
     public void testWriteDatabase(View view) {
-        myRef.setValue("Hansen");
+        myRef.setValue("Working");
     }
 
     public void testReadDatabase(View view) {
